c185ff1d24cee0a8b22d0e7ca5604f403e33ecfa
Evan Klitzke <evan@eklitzke.org>
2017-03-19 15:59:32 -0700

    .

--- vidextend/flow.py
+++ vidextend/flow.py
@@ -50,124 +50,18 @@ def should_include_image(path, start_num, end_num):
     return False
 
 
-class BasketballDirectoryIterator(keras.preprocessing.image.DirectoryIterator):
-    """hack"""
+def flow_from_directory(directory, a, b, c):
+    for dirpath, dirnames, fnames in os.walk(directory):
+        if len(dirnames) == 0:
+            # we are at a top-level directory, extract the images in our range
+            xs = []
+            ys = []
 
-    def __init__(self, directory, image_data_generator,
-                 target_size=(256, 256), color_mode='rgb',
-                 dim_ordering='default',
-                 classes=None, class_mode='categorical',
-                 batch_size=32, shuffle=True, seed=None,
-                 save_to_dir=None, save_prefix='', save_format='jpeg',
-                 follow_links=False,
-                 start_num=0,
-                 end_num=maxint):
-        if dim_ordering == 'default':
-            dim_ordering = K.image_dim_ordering()
-        self.directory = directory
-        self.image_data_generator = image_data_generator
-        self.target_size = tuple(target_size)
-        if color_mode not in {'rgb', 'grayscale'}:
-            raise ValueError('Invalid color mode:', color_mode,
-                             '; expected "rgb" or "grayscale".')
-        self.color_mode = color_mode
-        self.dim_ordering = dim_ordering
-        if self.color_mode == 'rgb':
-            if self.dim_ordering == 'tf':
-                self.image_shape = self.target_size + (3,)
-            else:
-                self.image_shape = (3,) + self.target_size
-        else:
-            if self.dim_ordering == 'tf':
-                self.image_shape = self.target_size + (1,)
-            else:
-                self.image_shape = (1,) + self.target_size
-        self.classes = classes
-        if class_mode not in {'categorical', 'binary', 'sparse', None}:
-            raise ValueError('Invalid class_mode:', class_mode,
-                             '; expected one of "categorical", '
-                             '"binary", "sparse", or None.')
-        self.class_mode = class_mode
-        self.save_to_dir = save_to_dir
-        self.save_prefix = save_prefix
-        self.save_format = save_format
-
-        white_list_formats = {'png', 'jpg', 'jpeg', 'bmp'}
-
-        # first, count the number of samples and classes
-        self.nb_sample = 0
-
-        if not classes:
-            classes = []
-            for subdir in sorted(os.listdir(directory)):
-                if os.path.isdir(os.path.join(directory, subdir)):
-                    classes.append(subdir)
-        self.nb_class = len(classes)
-        self.class_indices = dict(zip(classes, range(len(classes))))
-
-        def _recursive_list(subpath):
-            return sorted(os.walk(subpath, followlinks=follow_links), key=lambda tpl: tpl[0])
-
-        for subdir in classes:
-            subpath = os.path.join(directory, subdir)
-            for root, _, files in _recursive_list(subpath):
-                for fname in files:
-                    if should_include_image(fname, start_num, end_num):
-                        self.nb_sample += 1
-        print('Found %d images belonging to %d classes.' % (self.nb_sample, self.nb_class))
-
-        # second, build an index of the images in the different class subfolders
-        self.filenames = []
-        self.classes = np.zeros((self.nb_sample,), dtype='int32')
-        i = 0
-        for subdir in classes:
-            subpath = os.path.join(directory, subdir)
-            for root, _, files in _recursive_list(subpath):
-                for fname in files:
-                    if should_include_image(fname, start_num, end_num):
-                        self.classes[i] = self.class_indices[subdir]
-                        i += 1
-                        # add filename relative to directory
-                        absolute_path = os.path.join(root, fname)
-                        self.filenames.append(os.path.relpath(absolute_path, directory))
-        super(keras.preprocessing.image.DirectoryIterator, self).__init__(self.nb_sample, batch_size, shuffle, seed)
-
-
-class BasketballImageDataGenerator(keras.preprocessing.image.ImageDataGenerator):
-
-    def flow_from_directory(self, directory,
-                            target_size=(256, 256), color_mode='rgb',
-                            classes=None, class_mode='categorical',
-                            batch_size=32, shuffle=True, seed=None,
-                            save_to_dir=None,
-                            save_prefix='',
-                            save_format='jpeg',
-                            follow_links=False,
-                            start_num=0,
-                            end_num=maxint):
-
-        for dirpath, dirnames, fnames in os.walk(directory):
-            if len(dirnames) == 0:
-                # we are at a top-level directory, extract the images in our range
-                files_to_use = []
-                for fname in fnames:
-                    if should_include_image(fname, start_num, end_num):
-                        files_to_use.append(os.path.join(dirpath, fname))
-                files_to_use.sort()
-                yield np.asarray(hstack_images(files_to_use))
-#
-#        images = BasketballDirectoryIterator(
-#            directory, self,
-#            target_size=target_size, color_mode=color_mode,
-#            classes=classes, class_mode=class_mode,
-#            dim_ordering=self.dim_ordering,
-#            batch_size=batch_size, shuffle=shuffle, seed=seed,
-#            save_to_dir=save_to_dir,
-#            save_prefix=save_prefix,
-#            save_format=save_format,
-#            follow_links=follow_links,
-#            start_num=start_num,
-#            end_num=end_num)
-#
-#        # TODO: cache this
-#        yield np.asarray(hstack_images(images))
+            for fname in fnames:
+                if should_include_image(fname, a, b):
+                    xs.append(os.path.join(dirpath, fname))
+                elif should_include_image(fname, b + 1, c):
+                    ys.append(os.path.join(dirpath, fname))
+            xs.sort()
+            ys.sort()
+            yield (np.asarray(hstack_images(xs)), np.asarray(hstack_images(ys)))
