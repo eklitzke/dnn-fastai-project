45dfd80077690cf107c110be1df77af570978c6e
Evan Klitzke <evan@eklitzke.org>
2017-03-19 12:45:58 -0700

    try this

---
+++ flow.py
@@ -0,0 +1,134 @@
+import os.path
+import re
+import sys
+
+import keras.preprocessing.image
+
+NUM_RE = re.compile(r'(\d+)')
+
+
+WHITE_LIST_FORMATS = {'png', 'jpg', 'jpeg', 'bmp'}
+
+
+def should_include_image(path, start_num, end_num):
+    """Returns true if an image path should be included in the set, false
+    otherwise.
+    """
+    fname = os.path.filename(path).lower()
+
+    for extension in WHITE_LIST_FORMATS:
+        if fname.endswith('.' + extension):
+            num_match = NUM_RE.search(fname)
+            if num_match:
+                num, = num_match.groups()
+                num = int(num, 10)
+                return min_num <= num <= max_num
+    return False
+
+
+class BasketballDirectoryIterator(keras.processing.image.DirectoryIterator):
+    """hack"""
+
+    def __init__(self, directory, image_data_generator,
+                 target_size=(256, 256), color_mode='rgb',
+                 classes=None, class_mode='categorical',
+                 batch_size=32, shuffle=True, seed=None,
+                 data_format=None,
+                 save_to_dir=None, save_prefix='', save_format='jpeg',
+                 follow_links=False,
+                 start_num=0,
+                 end_num=sys.maxint):
+        if data_format is None:
+            data_format = K.image_data_format()
+        self.directory = directory
+        self.image_data_generator = image_data_generator
+        self.target_size = tuple(target_size)
+        if color_mode not in {'rgb', 'grayscale'}:
+            raise ValueError('Invalid color mode:', color_mode,
+                             '; expected "rgb" or "grayscale".')
+        self.color_mode = color_mode
+        self.data_format = data_format
+        if self.color_mode == 'rgb':
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (3,)
+            else:
+                self.image_shape = (3,) + self.target_size
+        else:
+            if self.data_format == 'channels_last':
+                self.image_shape = self.target_size + (1,)
+            else:
+                self.image_shape = (1,) + self.target_size
+        self.classes = classes
+        if class_mode not in {'categorical', 'binary', 'sparse', None}:
+            raise ValueError('Invalid class_mode:', class_mode,
+                             '; expected one of "categorical", '
+                             '"binary", "sparse", or None.')
+        self.class_mode = class_mode
+        self.save_to_dir = save_to_dir
+        self.save_prefix = save_prefix
+        self.save_format = save_format
+
+
+        # first, count the number of samples and classes
+        self.samples = 0
+
+        if not classes:
+            classes = []
+            for subdir in sorted(os.listdir(directory)):
+                if os.path.isdir(os.path.join(directory, subdir)):
+                    classes.append(subdir)
+        self.num_class = len(classes)
+        self.class_indices = dict(zip(classes, range(len(classes))))
+
+        def _recursive_list(subpath):
+            return sorted(os.walk(subpath, followlinks=follow_links), key=lambda tpl: tpl[0])
+
+        for subdir in classes:
+            subpath = os.path.join(directory, subdir)
+            for root, _, files in _recursive_list(subpath):
+                for fname in files:
+                    if should_include_image(fname, start_num, end_num):
+                        self.samples += 1
+        print('Found %d images belonging to %d classes.' % (self.samples, self.num_class))
+
+        # second, build an index of the images in the different class subfolders
+        self.filenames = []
+        self.classes = np.zeros((self.samples,), dtype='int32')
+        i = 0
+        for subdir in classes:
+            subpath = os.path.join(directory, subdir)
+            for root, _, files in _recursive_list(subpath):
+                for fname in files:
+                    if should_include_image(fname, start_num, end_num):
+                        self.classes[i] = self.class_indices[subdir]
+                        i += 1
+                        # add filename relative to directory
+                        absolute_path = os.path.join(root, fname)
+                        self.filenames.append(os.path.relpath(absolute_path, directory))
+        super(BasketballDirectoryIterator, self).__init__(self.samples, batch_size, shuffle, seed)
+
+
+class BasketballImageDataGenerator(keras.preprocessing.image.ImageDataGenerator):
+
+    def flow_from_directory(self, directory,
+                            target_size=(256, 256), color_mode='rgb',
+                            classes=None, class_mode='categorical',
+                            batch_size=32, shuffle=True, seed=None,
+                            save_to_dir=None,
+                            save_prefix='',
+                            save_format='jpeg',
+                            follow_links=False,
+                            start_num=0,
+                            end_num=sys.maxint):
+        return BasketballDirectoryIterator(
+            directory, self,
+            target_size=target_size, color_mode=color_mode,
+            classes=classes, class_mode=class_mode,
+            data_format=self.data_format,
+            batch_size=batch_size, shuffle=shuffle, seed=seed,
+            save_to_dir=save_to_dir,
+            save_prefix=save_prefix,
+            save_format=save_format,
+            follow_links=follow_links,
+            start_num=start_num,
+            end_num=end_num)
